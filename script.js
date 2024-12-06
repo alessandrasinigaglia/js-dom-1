8a29934e211365c176299aadd4532691ffe77188
alessandrasinigagliagit config --global user.email <you@example.com>
2024-12-06 14:00:41 +0100

    feat: add const

---
+++ script.js
@@ -0,0 +1,4 @@
+const lampOn = document.querySelector("Lamp-on");
+const lampOff = document.querySelector("Lamp-off");
+const btn = document.querySelector("btn");
+btn.addEventListener("click", function())
