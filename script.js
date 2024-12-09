de97960c8bc6d2188c0093e83f066271e195340f
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2024-12-09 12:50:02 +0100

    fix initial state

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const lampOn = document.getElementById("lamp-on");
 const lampOff = document.getElementById("lamp-off");
 const btn = document.getElementById("toggle-button");
+
+lampOn.classList.add("hidden");
+lampOff.classList.remove("hidden"); 
+btn.textContent = "ON"; 
+
 btn.addEventListener("click", function () {
     if (lampOn.classList.contains("hidden")) {
         lampOn.classList.remove("hidden");
